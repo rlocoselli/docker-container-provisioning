15c0fb9d62df0b51d8b44fc2266eaa8e4055ec6a
rlocoselli@outlook.fr <rlocoselli@outlook.fr>
2022-06-05 18:55:45 +0200

    Adding s3 bucket and adding readme file

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ variable "region" {
     default = "us-east-1"
 }
 
+variable "bucket_name" {}
+
+variable "acl" {
+    default = "private"
+}
+
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
@@ -71,5 +77,16 @@ resource "aws_instance" "mydockerapplication" {
 }
 
 output "mydockerapplication_public_ip" {
-    value = aws_instance.meubusao.public_ip
+    value = aws_instance.mydockerapplication.public_ip
 }
+
+resource "aws_s3_bucket" "application_s3_bucket" {
+    bucket = "${var.bucket_name}" 
+    acl = "${var.acl}"
+       
+}
+
+output "s3_output" {
+    value = aws_s3_bucket.application_s3_bucket.bucket
+}
+
